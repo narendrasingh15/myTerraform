0545be4eb983ee0b3bde3f05c32460a51b5e1b8d
Narendra Singh <38722887+narendrasingh15@users.noreply.github.com>
2019-06-27 14:16:27 +0530

    Delete ec2.tf

--- modules/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "naren_instance" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${var.subnet_id}"
-  tags = {
-    Name = "naren_instance"
-  }
-}
